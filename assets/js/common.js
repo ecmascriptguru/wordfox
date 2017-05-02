e6d2e23d00274ecfde74f8e39dd3677c6a157608
ecmascriptguru <ecmascript.guru@gmail.com>
2017-05-02 12:25:13 -0400

    keyword box event handled

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,10 +1,22 @@
 var WordFoxPro = (function() {
 	var _data = [],
+		_googleBaseUrl = "",
+		_amazonBaseUrl = "",
+		_dictionaryBaseUrl = "",
+		_cafeBaseUrl = "",
+
+		start = function(callback) {
+			if (typeof callback === "function") {
+				callback();
+			}
+		},
+		
 		init = function() {
 			console.log("initializing");
 		};
 
 	return {
-		init: init
+		init: init,
+		start: start
 	};
 })();
--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,9 +1,28 @@
 let Popup = (() => {
 	let _temp = [],
         _wordFoxPro = WordFoxPro,
+		_startButton = $("#btn-search"),
+		_keywordBox = $("#keyword"),
+
+		showResults = function() {
+			//
+		},
+
+		start = function() {
+			_wordFoxPro.start(showResults);
+		},
 
 		init = () => {
             _wordFoxPro.init();
+			_startButton.click(start);
+			_keywordBox.keydown(function(event) {
+				if (event.which == 13 || event.keyCode == 13) {
+					//code to execute here
+					start();
+				}
+				return true;
+			})
+
 			console.log("initializing...");
 		};
 
